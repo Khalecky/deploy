63b162724201ba83f4267cd300035e54cfb6d93e
romario <halecky@mail.ru>
2020-06-16 01:23:17 +0300

    9

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-SSH_ADDR=travis@$SERVER1;
+SSH_ADDR=travis@$1;
 echo $SSH_ADDR
 scp -r deploy $SSH_ADDR:~/
-ssh $SSH_ADDR "sh deploy/customize_config.sh $SERVER1"
+ssh $SSH_ADDR "sh deploy/customize_config.sh $1"
